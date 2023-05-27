c6e275f3b67f527f11d8471d8244414cecaba0d4
AmponsahJ99 <amjust99@gmail.com>
2023-05-26 18:23:22 -0700

    simple shell updated

--- s_tokenizer.c
+++ s_tokenizer.c
@@ -64,7 +64,7 @@ char **strtow2(char *str, char d)
 	for (i = 0; str[i] != '\0'; i++)
 		if ((str[i] != d && str[i + 1] == d) ||
 				    (str[i] != d && !str[i + 1]) || str[i + 1] == d)
-			numwords;
+			numwords++;
 
 	if (numwords == 0)
 		return (NULL);
